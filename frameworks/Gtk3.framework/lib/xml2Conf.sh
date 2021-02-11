f7c72769d14d369040fba76895e10597efb1da15
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-02-11 17:13:37 +0300

    Unused and useless Gtk3.framework deleted
    Now PokeChat is running only on pre-installed GTK3 version. Make sure you've downloaded it via homebrew etc.

--- frameworks/Gtk3.framework/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Library/Frameworks/Gtk3.framework/lib"
-XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm "
-XML2_INCLUDEDIR="-I/Library/Frameworks/Gtk3.framework/include/libxml2"
-MODULE_VERSION="xml2-2.8.0"
-
